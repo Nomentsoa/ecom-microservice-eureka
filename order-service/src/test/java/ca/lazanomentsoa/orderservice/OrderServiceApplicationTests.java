dc3edf3cd4deda8f27ed273bea601cc4209a7688
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2026-08-25 13:53:32 -0400

    project initialisaation

---
+++ order-service/src/test/java/ca/lazanomentsoa/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.lazanomentsoa.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
