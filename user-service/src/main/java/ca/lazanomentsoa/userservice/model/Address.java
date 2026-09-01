30f19527840e62105805bb172bd044c550943614
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2026-09-01 11:33:41 -0400

    add other files

--- user-service/src/main/java/ca/lazanomentsoa/userservice/model/Address.java
+++ user-service/src/main/java/ca/lazanomentsoa/userservice/model/Address.java
@@ -1,4 +1,22 @@
 package ca.lazanomentsoa.userservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
 }
--- user-service/src/main/java/ca/lazanomentsoa/userservice/model/User.java
+++ user-service/src/main/java/ca/lazanomentsoa/userservice/model/User.java
@@ -1,4 +1,41 @@
 package ca.lazanomentsoa.userservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity(name = "User")
+@NoArgsConstructor @AllArgsConstructor
+@DynamicUpdate
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private UserRole role = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 }
