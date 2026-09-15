c1c2602558cabc9b50b0f1154fcef9ae6bf0b7b9
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2026-09-15 15:13:47 -0400

    sfl4j init

--- user-service/src/main/java/ca/lazanomentsoa/userservice/controller/UserController.java
+++ user-service/src/main/java/ca/lazanomentsoa/userservice/controller/UserController.java
@@ -4,6 +4,9 @@ import ca.lazanomentsoa.dto.user.UserResponse;
 import ca.lazanomentsoa.userservice.dto.UserRequest;
 import ca.lazanomentsoa.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +14,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
+
+
+    // user of @slf4j
+//    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable("id") String id) {
+        log.info("Request received for user with id {}", id);
+        log.trace("Request received for user with id {}", id);
+        log.debug("Request received for user with id {}", id);
+        log.warn("Request received for user with id {}", id);
+        log.error("Request received for user with id {}", id);
         return userService.findUserById(Long.valueOf(id))
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
