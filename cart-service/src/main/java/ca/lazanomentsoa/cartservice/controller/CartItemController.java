aaa7e583e5e601055a3adc98a55126e5bd2ce081
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2026-09-22 14:11:20 -0400

    rate limiter resilience4J

--- cart-service/src/main/java/ca/lazanomentsoa/cartservice/controller/CartItemController.java
+++ cart-service/src/main/java/ca/lazanomentsoa/cartservice/controller/CartItemController.java
@@ -3,6 +3,8 @@ package ca.lazanomentsoa.cartservice.controller;
 import ca.lazanomentsoa.cartservice.dto.CreateItemRequest;
 import ca.lazanomentsoa.cartservice.model.CartItem;
 import ca.lazanomentsoa.cartservice.service.CartItemService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +23,13 @@ public class CartItemController {
             return ResponseEntity.ok(response);
     }
 
+    @RateLimiter(name = "rateLimiterBreaker", fallbackMethod = "getMessageFallback")
     @GetMapping("/test")
     public ResponseEntity<String> test(){
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.OK).body("Hello Cart Service");
+    }
+
+    public ResponseEntity<String> getMessageFallback(Exception exception){
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Hello Fallback RateLimiter");
     }
 }
